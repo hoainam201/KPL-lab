fa133c52c057c6018514adcf7cb848f9c19c67f2
hoainam2404 <85448409+hoainam2404@users.noreply.github.com>
2022-04-27 16:54:14 +0700

    W3

---
+++ W3/cmake-build-debug/CMakeFiles/W3.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/CTD/KPL-lab/W3/charcode.c" "D:/CTD/KPL-lab/W3/cmake-build-debug/CMakeFiles/W3.dir/charcode.c.obj"
+  "D:/CTD/KPL-lab/W3/error.c" "D:/CTD/KPL-lab/W3/cmake-build-debug/CMakeFiles/W3.dir/error.c.obj"
+  "D:/CTD/KPL-lab/W3/main.c" "D:/CTD/KPL-lab/W3/cmake-build-debug/CMakeFiles/W3.dir/main.c.obj"
+  "D:/CTD/KPL-lab/W3/parser.c" "D:/CTD/KPL-lab/W3/cmake-build-debug/CMakeFiles/W3.dir/parser.c.obj"
+  "D:/CTD/KPL-lab/W3/reader.c" "D:/CTD/KPL-lab/W3/cmake-build-debug/CMakeFiles/W3.dir/reader.c.obj"
+  "D:/CTD/KPL-lab/W3/scanner.c" "D:/CTD/KPL-lab/W3/cmake-build-debug/CMakeFiles/W3.dir/scanner.c.obj"
+  "D:/CTD/KPL-lab/W3/token.c" "D:/CTD/KPL-lab/W3/cmake-build-debug/CMakeFiles/W3.dir/token.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ W3/cmake-build-debug/CMakeFiles/W3.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/W3.dir/charcode.c.obj"
+  "CMakeFiles/W3.dir/error.c.obj"
+  "CMakeFiles/W3.dir/main.c.obj"
+  "CMakeFiles/W3.dir/parser.c.obj"
+  "CMakeFiles/W3.dir/reader.c.obj"
+  "CMakeFiles/W3.dir/scanner.c.obj"
+  "CMakeFiles/W3.dir/token.c.obj"
+  "W3.exe"
+  "W3.exe.manifest"
+  "W3.pdb"
+  "libW3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/W3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
