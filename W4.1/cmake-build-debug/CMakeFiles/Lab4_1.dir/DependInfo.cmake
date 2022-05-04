8198d2d87c61a8634d62fe12cd75b63a45e7174f
hoainam2404 <85448409+hoainam2404@users.noreply.github.com>
2022-05-04 19:53:45 +0700

    W4

---
+++ W4.1/cmake-build-debug/CMakeFiles/Lab4_1.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/CTD/KPL-lab/Lab4.1/debug.c" "D:/CTD/KPL-lab/Lab4.1/cmake-build-debug/CMakeFiles/Lab4_1.dir/debug.c.obj"
+  "D:/CTD/KPL-lab/Lab4.1/main.c" "D:/CTD/KPL-lab/Lab4.1/cmake-build-debug/CMakeFiles/Lab4_1.dir/main.c.obj"
+  "D:/CTD/KPL-lab/Lab4.1/symtab.c" "D:/CTD/KPL-lab/Lab4.1/cmake-build-debug/CMakeFiles/Lab4_1.dir/symtab.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ W4.1/cmake-build-debug/CMakeFiles/Lab4_1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab4_1.dir/debug.c.obj"
+  "CMakeFiles/Lab4_1.dir/main.c.obj"
+  "CMakeFiles/Lab4_1.dir/symtab.c.obj"
+  "Lab4_1.exe"
+  "Lab4_1.exe.manifest"
+  "Lab4_1.pdb"
+  "libLab4_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab4_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
