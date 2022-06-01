0b29fdd56fe04e2762f1d8d4b8755b7246269c3c
hoainam2404 <85448409+hoainam2404@users.noreply.github.com>
2022-06-01 15:43:31 +0700

    Update

---
+++ W4.3/cmake-build-debug/CMakeFiles/W4_3.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/CTD/KPL-lab/W4.3/charcode.c" "D:/CTD/KPL-lab/W4.3/cmake-build-debug/CMakeFiles/W4_3.dir/charcode.c.obj"
+  "D:/CTD/KPL-lab/W4.3/debug.c" "D:/CTD/KPL-lab/W4.3/cmake-build-debug/CMakeFiles/W4_3.dir/debug.c.obj"
+  "D:/CTD/KPL-lab/W4.3/error.c" "D:/CTD/KPL-lab/W4.3/cmake-build-debug/CMakeFiles/W4_3.dir/error.c.obj"
+  "D:/CTD/KPL-lab/W4.3/main.c" "D:/CTD/KPL-lab/W4.3/cmake-build-debug/CMakeFiles/W4_3.dir/main.c.obj"
+  "D:/CTD/KPL-lab/W4.3/parser.c" "D:/CTD/KPL-lab/W4.3/cmake-build-debug/CMakeFiles/W4_3.dir/parser.c.obj"
+  "D:/CTD/KPL-lab/W4.3/reader.c" "D:/CTD/KPL-lab/W4.3/cmake-build-debug/CMakeFiles/W4_3.dir/reader.c.obj"
+  "D:/CTD/KPL-lab/W4.3/scanner.c" "D:/CTD/KPL-lab/W4.3/cmake-build-debug/CMakeFiles/W4_3.dir/scanner.c.obj"
+  "D:/CTD/KPL-lab/W4.3/semantics.c" "D:/CTD/KPL-lab/W4.3/cmake-build-debug/CMakeFiles/W4_3.dir/semantics.c.obj"
+  "D:/CTD/KPL-lab/W4.3/symtab.c" "D:/CTD/KPL-lab/W4.3/cmake-build-debug/CMakeFiles/W4_3.dir/symtab.c.obj"
+  "D:/CTD/KPL-lab/W4.3/token.c" "D:/CTD/KPL-lab/W4.3/cmake-build-debug/CMakeFiles/W4_3.dir/token.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ W4.3/cmake-build-debug/CMakeFiles/W4_3.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/W4_3.dir/charcode.c.obj"
+  "CMakeFiles/W4_3.dir/debug.c.obj"
+  "CMakeFiles/W4_3.dir/error.c.obj"
+  "CMakeFiles/W4_3.dir/main.c.obj"
+  "CMakeFiles/W4_3.dir/parser.c.obj"
+  "CMakeFiles/W4_3.dir/reader.c.obj"
+  "CMakeFiles/W4_3.dir/scanner.c.obj"
+  "CMakeFiles/W4_3.dir/semantics.c.obj"
+  "CMakeFiles/W4_3.dir/symtab.c.obj"
+  "CMakeFiles/W4_3.dir/token.c.obj"
+  "W4_3.exe"
+  "W4_3.exe.manifest"
+  "W4_3.pdb"
+  "libW4_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/W4_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
